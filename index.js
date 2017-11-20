2a2940e69ce44329f1c34fcf006bc4a245fecbf4
grneggandsam <samuel.louis.milligan@gmail.com>
2017-11-19 23:32:58 -0600

    Refactored to include socket.io

---
+++ index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var app = express();
+var server = app.listen(3000, function() {
+    console.log("server started on port 3000");
+});
+var io = require('socket.io').listen(server);
+var session = require('express-session')
+var cookieParser = require('cookie-parser')
+
+var numConnected = 0;
+var mapData = {
+  curId: 0,
+  units: {}
+};
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    mapData.units[msg.id] = msg;
+    io.emit('chat message', mapData);
+  });
+  numConnected++;
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+
+    numConnected--;
+  });
+});
+
+app.use(session({
+  secret: 'samwise',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(cookieParser());
+
+// set up static file serving from the public directory
+app.use('/static', express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  if(!req.session.user) {
+    mapData.curId++;
+    req.session.user = mapData.curId;
+    res.cookie('userId', req.session.user);
+  }
+  res.sendFile(__dirname + '/index.html');
+});
---
+++ public/javascripts/navigation.js
@@ -0,0 +1,234 @@
+
+
+var X = 500;
+var Y = 350;
+var s = 5;
+var action = 0;
+gameplay = true;
+const vWidth = 1100;
+const vHeight = 800;
+const cX = (vWidth/2-50);
+const cY = (vHeight/2-50);
+var domMAP = Array(rows).fill().map(() => Array(columns));
+var dude = new Image();
+var c;
+var ctx;
+var flipped = false;
+
+initiateMap();
+var socket = io();
+var id;
+var outsideData;
+
+
+$( function() {
+  id = docCookies.getItem('userId');
+  socket.on('chat message', function(msg){
+      outsideData = msg;
+      drawMap();
+  });
+  c = document.getElementById("theView");
+  ctx = c.getContext("2d");
+  dude.src = '/static/images/still.png';
+  drawMap();
+  window.requestAnimationFrame(render);
+});
+
+function render() {
+  switch(action) {
+    case 1:
+      if(canGo(X, Y-s)) {
+        Y-=s;
+      }
+      break;
+    case 2:
+      if(canGo(X, Y+s)) {
+        Y+=s;
+      }
+      break;
+    case 3:
+      if(canGo(X-s, Y)) {
+        X-=s;
+        flipped = true;
+      }
+      break;
+    case 4:
+      if(canGo(X+s, Y)) {
+        X+=s;
+        flipped = false;
+      }
+      break;
+  };
+
+  if(action != 0) {
+    ctx.clearRect(0, 0, vWidth, vHeight); // clear canvas
+    if(flipped) {
+      dude.src = '/static/images/stillF.png';
+    }
+    else {
+      dude.src = '/static/images/still.png';
+    }
+    drawMap();
+    var data = {id: id, x: X, y: Y};
+    socket.emit('chat message', data);
+  }
+  window.requestAnimationFrame(render);
+}
+
+function canGo(iX, iY) {
+  if(MAP[parseInt(iX/100)][parseInt(iY/100)] != 0 || MAP[parseInt((iX+100)/100)][parseInt((iY+100)/100)] != 0) {
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
+function drawMap() {
+  let x0 = 500-X;
+  let y0 = 350-Y;
+  var grd = ctx.createLinearGradient(0,0,200,0);
+  for(let i=leftBound(); i<rightBound(); i++) {
+    for(let j=topBound(); j<bottomBound(); j++) {
+      if(MAP[i][j] == 1) {
+        wallTile(i*100+x0, j*100+y0);
+      }
+      else {
+        grassTile(i*100+x0,j*100+y0);
+      }
+    }
+  }
+  if(outsideData != null) {
+    if(outsideData.units != null) {
+      var keys = Object.keys(outsideData.units);
+      for(var i=0;i<keys.length;i++){
+          var key = keys[i];
+          // console.log(key, outsideData.units[key].x);
+          // console.log(X);
+          ctx.drawImage(dude, outsideData.units[key].x - X + 500, outsideData.units[key].y - Y + 350, 100, 100);
+      }
+    }
+  }
+  // console.log(outsideData);
+  // ctx.drawImage(dude, cX, cY, 100, 100);
+}
+
+function drawDude(x, y) {
+
+}
+
+function grassTile(x, y) {
+  ctx.fillStyle = '#5a2';
+  ctx.fillRect(x,y,100, 100);
+  ctx.beginPath();
+  ctx.moveTo(x+10, y+10);
+  ctx.lineTo(x+20, y+30);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(x+30, y+10);
+  ctx.lineTo(x+20, y+30);
+  ctx.stroke();
+}
+
+function wallTile(x, y) {
+  ctx.fillStyle = '#aaa';
+  ctx.fillRect(x, y,100, 100);
+}
+
+function leftBound() {
+  if(X-500 < 0)
+    return 0;
+  else {
+    return parseInt((X-500)/100);
+  }
+}
+
+function rightBound() {
+  if(parseInt((X+500)/100) + 2 > columns)
+    return (columns);
+  return parseInt((X+500)/100) + 2;
+}
+
+function topBound() {
+  if(Y-350 < 0)
+    return 0;
+  else {
+    return parseInt((Y-350)/100);
+  }
+}
+
+function bottomBound() {
+  if(parseInt((Y+350)/100) + 2 > rows)
+    return rows;
+  return parseInt((Y+350)/100) + 2;
+}
+
+function colorMap(callback) {
+  for(let i=0; i<(rows); i++) {
+    for(let j=0; j<(columns); j++) {
+      if(MAP[i][j] == 1) {
+        ctx.fillStyle = '#eee';
+        ctx.fillRect(10+x0,10+y0,150, 80);
+      }
+      else {
+
+      }
+    }
+  }
+  callback();
+}
+
+function initiateDomMap(callback) {
+  for(let i=0; i<(rows); i++) {
+    for(let j=0; j<(columns); j++) {
+      var element = document.createElement("div");
+      element.id= "x" + i + "y" + j;
+      element.className = "mapBlock";
+      var blah = document.getElementById("theMap").appendChild(element);
+      // console.log(blah);
+    }
+  }
+  callback();
+}
+
+document.addEventListener('keydown', (event) => {
+  const keyName = event.key;
+  if(action == 0) {
+    switch(keyName) {
+      case "ArrowUp":
+        goUp();
+        break;
+      case "ArrowDown":
+        goDown();
+        break;
+      case "ArrowRight":
+        goRight();
+        break;
+      case "ArrowLeft":
+        goLeft();
+        break;
+    };
+  };
+  // emit socket data for movement
+  // socket.emit('chat message', 'sending something');
+});
+
+document.addEventListener('keyup', (event) => {
+  action = 0;
+});
+
+function goUp() {
+  action = 1;
+};
+
+function goDown() {
+  action = 2;
+};
+
+function goLeft() {
+  action = 3;
+};
+
+function goRight() {
+  action = 4;
+};
